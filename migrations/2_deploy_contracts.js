763b68e3689d0ff8d59185426b2ae3d415e84151
Aditya yadav <heyaadi2@gmail.com>
2024-03-15 00:02:56 +0530

    adding environment

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const DeBook = artifacts.require("DeBook");
+
+module.exports = function(deployer) {
+  // Code goes here...
+};
---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import Identicon from 'identicon.js';
+import './App.css';
+import DeBook from '../abis/DeBook.json'
+import Navbar from './Navbar'
+import Main from './Main'
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar account={this.state.account} />
+        { this.state.loading
+          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+          : <Main
+            // Code...
+            />
+          }
+      </div>
+    );
+  }
+}
+
+export default App;
